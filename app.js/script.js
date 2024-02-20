6fd4f839a75d98e9b21ae52ee4ae163213c66d42
Sebastian <sebastianlakla@gmail.com>
2024-02-20 19:34:25 +0100

    change classes in html structure with custom button

--- app.js/script.js
+++ app.js/script.js
@@ -1,6 +1,7 @@
 "use strict";
 const billAmount = document.querySelector(".price-area__input");
 const tipButtons = document.querySelectorAll(".tip-buttons-area__button");
+const customButton = document.querySelector('.tip-buttons-area__custom')
 const error = document.querySelector(".people-area-heading__p-error");
 const numOfPeople = document.querySelector(".amount-people__input");
 const tipAmount = document.querySelector(".tip-amount-person__amount");
@@ -10,9 +11,16 @@ const resetButton = document.querySelector(".tip-info__cta");
 console.log(
   billAmount,
   tipButtons,
+  customButton,
   error,
   numOfPeople,
   tipAmount,
   totalAmount,
   resetButton
 );
+resetButton.addEventListener("click", () => {
+  billAmount.value = "";
+  numOfPeople.value = "";
+  tipAmount.textContent = "$ 0.00";
+  totalAmount.textContent = "$ 0.00";
+});
