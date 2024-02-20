0e29e322de39b9eb96801e35e4e3072a049de0d9
Sebastian <sebastianlakla@gmail.com>
2024-02-20 18:57:19 +0100

    Get all elements to work in script

--- app.js/script.js
+++ app.js/script.js
@@ -1,1 +1,18 @@
-'use strict';
+"use strict";
+const billAmount = document.querySelector(".price-area__input");
+const tipButtons = document.querySelectorAll(".tip-buttons-area__button");
+const error = document.querySelector(".people-area-heading__p-error");
+const numOfPeople = document.querySelector(".amount-people__input");
+const tipAmount = document.querySelector(".tip-amount-person__amount");
+const totalAmount = document.querySelector(".tip-total-person__amount");
+const resetButton = document.querySelector(".tip-info__cta");
+
+console.log(
+  billAmount,
+  tipButtons,
+  error,
+  numOfPeople,
+  tipAmount,
+  totalAmount,
+  resetButton
+);
