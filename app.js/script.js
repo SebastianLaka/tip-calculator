52d24c335ad0a448391fcb499ad1d785d84373a4
Sebastian <sebastianlakla@gmail.com>
2024-02-25 13:34:01 +0100

    Add tip value after amounts in inputs

--- app.js/script.js
+++ app.js/script.js
@@ -8,29 +8,43 @@ const tipAmount = document.querySelector(".tip-amount-person__amount");
 const totalAmount = document.querySelector(".tip-total-person__amount");
 const resetButton = document.querySelector(".tip-info__cta");
 
+// const x = (billAmount.value = 142.55);
+// const y = (numOfPeople.value = 5);
+// const f = 15;
+// const z = x / y / f;
+// console.log(z);
 
 const main = () => {
-tipButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-   const getNumber = Number(button.value);
-   console.log(getNumber);
+  tipButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const getNumber = Number(button.value);
+      console.log(getNumber);
+      const getTotalTipValue = () => {
+        const amountOfTip = billAmount.value / numOfPeople.value;
+
+        totalAmount.textContent = `$ ${amountOfTip}`;
+        // totalAmount.textContent = `${a}`;
+      };
+      if (billAmount.value > 0 && numOfPeople.value > 0) {
+        getTotalTipValue();
+      }
+    });
   });
-});
-const errorRemove = () => {
-  if(Number(numOfPeople.value) > 0){ 
-    error.classList.toggle("hidden");
-  }
+  const errorRemove = () => {
+    if (Number(numOfPeople.value) > 0) {
+      error.classList.toggle("hidden");
+    }
+  };
+  numOfPeople.addEventListener("click", (e) => {
+    e.preventDefault();
+    errorRemove();
+  });
+  resetButton.addEventListener("click", () => {
+    billAmount.value = "";
+    customButton.value = "";
+    numOfPeople.value = "";
+    tipAmount.textContent = "$ 0.00";
+    totalAmount.textContent = "$ 0.00";
+  });
 };
-numOfPeople.addEventListener("click", (e) => {
-  e.preventDefault();
-  errorRemove();
-});
-resetButton.addEventListener("click", () => {
-  billAmount.value = "";
-  customButton.value = "";
-  numOfPeople.value = "";
-  tipAmount.textContent = "$ 0.00";
-  totalAmount.textContent = "$ 0.00";
-});
-}
 main();
